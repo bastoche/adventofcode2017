38a362671ea9ee42d9a887fab2abbe3c402a6cbe
Bastien Duret <bastien.duret@gmail.com>
2017-12-17 17:10:34 +0100

    Add day9 part two

--- day_9.py
+++ day_9.py
@@ -1,5 +1,16 @@
 def part_one(input):
+    score, _ = process_input(input)
+    return score
+
+
+def part_two(input):
+    _, cleaned_garbage = process_input(input)
+    return cleaned_garbage
+
+
+def process_input(input):
     score = 0
+    cleaned_garbage = 0
     depth = 0
     garbage = False
     cancel = False
@@ -12,6 +23,7 @@ def part_one(input):
         elif char == '>':
             garbage = False
         elif garbage:
+            cleaned_garbage += 1
             continue
         elif char == '<':
             garbage = True
@@ -20,10 +32,11 @@ def part_one(input):
         elif char == '}':
             score += depth
             depth -= 1
-    return score
+    return score, cleaned_garbage
 
 
 if __name__ == "__main__":
     with open('day_9.txt') as file:
         input = file.read()
     print(part_one(input))
+    print(part_two(input))
--- test_day_9.py
+++ test_day_9.py
@@ -1,4 +1,4 @@
-from day_9 import part_one
+from day_9 import part_one, part_two
 
 
 def test_part_one():
@@ -10,3 +10,13 @@ def test_part_one():
     assert part_one('{{<ab>},{<ab>},{<ab>},{<ab>}}') == 9
     assert part_one('{{<!!>},{<!!>},{<!!>},{<!!>}}') == 9
     assert part_one('{{<a!>},{<a!>},{<a!>},{<ab>}}') == 3
+
+
+def test_part_two():
+    assert part_two('<>') == 0
+    assert part_two('<random characters>') == 17
+    assert part_two('<<<<>') == 3
+    assert part_two('<{!>}>') == 2
+    assert part_two('<!!>') == 0
+    assert part_two('<!!!>>') == 0
+    assert part_two('<{o"i!a,<{i<a>') == 10
