0fc14a04d7fc86995040cd4ffe0495c9fa928e82
Bastien Duret <bastien.duret@gmail.com>
2017-12-23 19:13:43 +0100

    Add day 10 part two

--- day_10.py
+++ day_10.py
@@ -1,17 +1,25 @@
+from functools import reduce
+
+
 def part_one(input, size=256):
     input_list = map(int, input.split(','))
-    elements = list(range(0, size))
-    start = 0
+    elements = list(range(size))
+    current_position = 0
     skip_size = 0
-    for length in input_list:
-        elements = invert_sublist(elements, start, length)
-        start += (length + skip_size) % len(elements)
-        skip_size += 1
+    elements, _, _ = run_round(input_list, elements, current_position, skip_size)
     return elements[0] * elements[1]
 
 
+def run_round(input_list, elements, current_position, skip_size):
+    for length in input_list:
+        elements = invert_sublist(elements, current_position, length)
+        current_position += (length + skip_size) % len(elements)
+        skip_size += 1
+    return elements, current_position, skip_size
+
+
 def invert_sublist(elements, start, length):
-    for i in range(0, length // 2):
+    for i in range(length // 2):
         swap(elements, (start + i) % len(elements), (start + length - 1 - i) % len(elements))
     return elements
 
@@ -23,5 +31,38 @@ def swap(elements, leftIndex, rightIndex):
     return elements
 
 
+def part_two(input, size=256):
+    ascii_codes = to_ascii_codes(input)
+    sequence = ascii_codes + [17, 31, 73, 47, 23]
+    current_position = 0
+    skip_size = 0
+    elements = list(range(size))
+    for i in range(64):
+        elements, current_position, skip_size = run_round(sequence, elements, current_position, skip_size)
+    dense_hash = to_dense_hash(elements)
+    return to_hexadecimal_string(dense_hash)
+
+
+def to_ascii_codes(input):
+    return [ord(character) for character in input]
+
+
+def to_dense_hash(input):
+    length = len(input) // 16
+    result = []
+    for i in range(length):
+        elements = [input[i * 16 + j] for j in range(16)]
+        result.append(reduce(lambda a,b: a ^ b, elements))
+    return result
+
+
+def to_hexadecimal_string(input):
+    result = ''
+    for element in input:
+        result += f'{element:02x}'
+    return result
+
+
 if __name__ == "__main__":
     print(part_one('102,255,99,252,200,24,219,57,103,2,226,254,1,0,69,216'))
+    print(part_two('102,255,99,252,200,24,219,57,103,2,226,254,1,0,69,216'))
--- test_day_10.py
+++ test_day_10.py
@@ -1,4 +1,4 @@
-from day_10 import part_one, invert_sublist, swap
+from day_10 import part_one, invert_sublist, swap, part_two, to_ascii_codes, to_dense_hash, to_hexadecimal_string
 
 
 def test_part_one():
@@ -14,3 +14,24 @@ def test_invert_sublist():
 
 def test_swap():
     assert swap([0, 1, 2, 3, 4], 0, 2) == [2, 1, 0, 3, 4]
+
+
+def test_part_two():
+    assert part_two('') == 'a2582a3a0e66e6e86e3812dcb672a272'
+    assert part_two('AoC 2017') == '33efeb34ea91902bb2f59c9920caa6cd'
+    assert part_two('1,2,3') == '3efbe78a8d82f29979031a4aa0b16a9d'
+    assert part_two('1,2,4') == '63960835bcdc130f0b66d7ff4f6a5a8e'
+
+
+def test_to_ascii_codes():
+    assert to_ascii_codes('1,2,3') == [49, 44, 50, 44, 51]
+
+
+def test_to_dense_hash():
+    assert to_dense_hash([65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]) == [64]
+    assert to_dense_hash([0] * 256) == [0] * 16
+    assert to_dense_hash([65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22] * 16) == [64] * 16
+
+
+def test_to_hexadecimal_string():
+    assert to_hexadecimal_string([64, 7, 255]) == '4007ff'
